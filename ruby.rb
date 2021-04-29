6ec38dcf9028563a6dfc9864167702f477f30391
tomo_kamaji <tomoya.kamaji@gmail.com>
2021-04-29 10:28:48 +0900

    最初のプッシュ

--- ruby.rb
+++ ruby.rb
@@ -1,1 +1,25 @@
-puts "Hello, world!"
+# bingoJudgeCard = ['apple', 'orange', 'cube'];
+
+
+selected = [];
+S = 3;
+N = 5;
+
+# bingoCard[0] = ['apple', 'orange', 'cube'];
+# bingoCard[1] = ['batch', 'web', 'cloud'];
+# bingoCard[2] = ['sql', 'http', 'https'];
+
+
+# ビンゴの判定用のカード作成
+bingoJudgeCard = Array.new(S, Array.new(S, "0") )
+
+for i in 1..S do
+  for j in 1..S do
+    # print(bingoJudgeCard[i][j]);
+  end
+end
+
+
+
+
+print(bingoJudgeCard);
