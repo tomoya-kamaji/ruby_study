ac5fd32dd4aa40b55dddf295531cd7ff490f7f4e
tomo_kamaji <tomoya.kamaji@gmail.com>
2021-04-29 16:25:00 +0900

    縦,横ビンゴ完成

--- ruby.rb
+++ ruby.rb
@@ -5,6 +5,7 @@
   判定用ビンゴカードに1を入れる
 =end
 
+
 =begin
   ビンゴの位置を探す関数
   IN
@@ -12,7 +13,7 @@
     $selected：選ばれたワード
   OUT
     ビンゴが存在する位置(i,j)
-    存在しなければnull
+    存在しなければnill
 =end
 def find_position(bingoCard, selected)
   bingoCard.each_with_index do |column,i|
@@ -24,6 +25,47 @@ def find_position(bingoCard, selected)
     end
   end
   return nill;
+end
+
+=begin
+  横のビンゴ判定
+    ”横の最初の数が1”かつ”横が全て重複”していたらビンゴ。これを行数分繰り返す。
+  IN
+    bingoJudgeCard
+  OUT
+    ビンゴならtrue。ハズレはfalse
+=end
+def bingo_row(bingoJudgeCard)
+  bingoJudgeCard.each do |column|
+    if column[0] == 1 && column.uniq.size == 1 then
+      return true
+    end
+  end
+  return false
+end
+
+=begin
+  縦のビンゴ判定
+    横のビンゴ判定を再利用するため、縦横を置換。その後、横判定を利用
+  IN
+    bingoJudgeCard
+  OUT
+    ビンゴならtrue。ハズレはfalse
+=end
+def bingo_column(bingoJudgeCard)
+  replace_bingoJudgeCard = Array.new(S).map{Array.new(S,0) }
+  bingoJudgeCard.each_with_index do |column,i|
+    column.each_with_index do |value,j|
+      replace_bingoJudgeCard[j][i] = value
+    end
+  end
+  p replace_bingoJudgeCard
+  replace_bingoJudgeCard.each do |column|
+    if column[0] == 1 && column.uniq.size == 1 then
+      return true
+    end
+  end
+  return false
 end
 
 S = 3;
@@ -40,15 +82,20 @@ selected_naname = ['q', 's', 'c'];
 selected_false = ['q', 'z', 't'];
 
 # ビンゴの判定用のカード作成
-bingoJudgeCard = Array.new(S, Array.new(S, "0") )
 bingoJudgeCard = Array.new(S).map{Array.new(S,0) }
 
-
-# 二次元配列で
+# ワードが存在すれば1,そうでなければ0
 selected_row.each do |selected|
   position = find_position(bingoCard, selected)
-  
   bingoJudgeCard[position[0]][position[1]] = 1;
 end
 
 p bingoJudgeCard
+
+isRow = bingo_row(bingoJudgeCard)
+isColumn = bingo_column(bingoJudgeCard)
+
+puts isRow
+puts isColumn
+
+
